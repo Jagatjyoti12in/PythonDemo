eb33d37b71ee256f09d5a840e4fa412505e2c0ef
Jagatjyoti Mohanty <jagatjyoti007@gmail.com>
2026-03-18 16:24:00 +0530

    Add files via upload

---
+++ wite a pyton program to add two numbers-using cursor- and calculator.py
@@ -0,0 +1,17 @@
+
+
+#write a calculator app for me in python
+
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Calculator")
+root.geometry("300x200")
+
+label = tk.Label(root, text="Calculator")
+label.pack()
+
+root.mainloop()
+
+# 
